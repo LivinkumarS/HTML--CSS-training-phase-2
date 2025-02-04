32474511563efc58eb677a41c4136c66ad05c25c
LivinkumarS <001liv100@ggmail.com>
2025-02-04 13:18:00 +0530

    arithmetic, string operations

--- app.js
+++ app.js
@@ -2,4 +2,37 @@
 // number1 = 90;
 // console.log(number1);
 
-alert("Welcome to my website!")
+let a = 10;
+let b = 1;
+
+// a = a + 1;
+// a++;  //11 (10+1)
+// ++a;  //11 (10+1)
+
+// const c = ++a; //a=11, c=11 pre
+const c = a++; //c=10,a=11 post
+
+console.log("a:", a);
+console.log("c:", c);
+
+// String Operations
+
+// //concatination
+
+let first_name = "pratap";
+let last_name = "pojari";
+
+let name = first_name + " " + last_name;
+
+console.log(name);
+
+//length
+
+console.log(first_name.length);
+
+// Slice
+
+const str = "banana and apple I would love to eat";
+
+let newStr = str.slice(8, 17);
+console.log(newStr);
