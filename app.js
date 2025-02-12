4c79fd04b97a9e4df9d66f38c1418ce610b94a27
LivinkumarS <001liv100@ggmail.com>
2025-02-12 12:20:48 +0530

    Nested If

--- app.js
+++ app.js
@@ -1,53 +1,28 @@
-// let a=10;
+let i = 11;
 
-// if(a<10){
-//     console.log("surya");
-//     console.log("pratap");
-// }else{
-//     console.log("Livin");
-// }
+//nested if
 
-// let yes = prompt();
-
-// console.log("yes:",typeof );
-
-// let userAge=17;
-
-// if (userAge>= 18) {
-//   console.log("Adult");
+// if (i < 10) {
+//   if (i < 5) {
+//     console.log("very less");
+//   } else {
+//     console.log("less");
+//   }
 // } else {
-//   console.log("child");
+//   console.log("Higher value");
 // }
 
-// BMI Calculator
+//else if
 
-let weight = 50;
-let height = 1.8;
-
-let BMI = weight / (height * height);
-
-console.log(BMI); 
-
-if (BMI < 14) {
-  console.log("consult doc");
-} else if (BMI < 18) {
-  console.log("under weight");
-} else if (BMI > 30) {
-  console.log("Visit Doc");
-} else if (BMI > 25) {
-  console.log("Obese");
+if (i < 5) {
+  console.log("very less");
+} else if (i <= 10) {
+  console.log("less");
 } else {
-  console.log("Just Correct");
+  console.log("Higher value");
 }
 
+// i<5---> print "very less"
+// 5,6,7,8,9,10---> print "less"
 
-
-let mark=90;
-
-// 90 or above 90--->A+
-// 80 to 89---->A
-// 70  to 79--->B+
-// 60 to 69---->B
-// 50 to 59---->C
-
-// below 50---> fail
+// 11,12...,10<i----> print "higher value"
