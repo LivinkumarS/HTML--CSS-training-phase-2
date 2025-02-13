9367998027066f3df3ffc3d2530c03820581cefc
LivinkumarS <001liv100@ggmail.com>
2025-02-13 11:37:39 +0530

    Object- Bsasics

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-function sendBack() {
-  console.log("First");
-  console.log("Second");
-  console.log("Third");
-  return true;
-}
+let person = {
+  name: "Pratap",
+  Role: "Developer",
+  Company: "TCS",
+  Qualification: "BE",
+  age: 28,
+  isMarried: false,
 
-// let value=sendBack();
+  printFullname: function () {
+    console.log("Pratap Poojari");
+  },
+  printRole: () => {
+    console.log("Developer in TCS");
+  },
+};
 
-// console.log("Returned Value: ",value);
 
-function sqr(num) {
-  return num * num;
-}
 
-function BMIcalc(weight, height) {
-  let BMI = weight / sqr(height);
-  console.log(BMI);
-  if (BMI < 14) {
-    console.log("consult doc");
-  } else if (BMI < 18) {
-    console.log("under weight");
-  } else if (BMI > 30) {
-    console.log("Visit Doc");
-  } else if (BMI > 25) {
-    console.log("Obese");
-  } else {
-    console.log("Just Correct");
-  }
-}
+//Dot
+// console.log(person.name);
+// console.log(person.Qualification);
 
-BMIcalc(68,1.8)
+// //sqr brackets
+// console.log(person["name"]);
+// console.log(person["Qualification"]);
+person.age--;
+person.printFullname();
+person.printRole();
