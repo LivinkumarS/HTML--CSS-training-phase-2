adf0180e387ef29bbc7620bb81abc7689499cfae
LivinkumarS <001liv100@ggmail.com>
2025-02-13 11:14:25 +0530

    Function-Return

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-// function add(num1, num2) {
-//   console.log(num1 + num2);
-// }
+function sendBack() {
+  console.log("First");
+  console.log("Second");
+  console.log("Third");
+  return true;
+}
 
-// function mul(num1, num2) {
-//   console.log(num1 * num2);
-// }
+// let value=sendBack();
 
-// function BMIcalc(weight, height) {
-// let BMI = weight / (height * height);
+// console.log("Returned Value: ",value);
 
-// console.log(BMI);
+function sqr(num) {
+  return num * num;
+}
 
-// if (BMI < 14) {
-//   console.log("consult doc");
-// } else if (BMI < 18) {
-//   console.log("under weight");
-// } else if (BMI > 30) {
-//   console.log("Visit Doc");
-// } else if (BMI > 25) {
-//   console.log("Obese");
-// } else {
-//   console.log("Just Correct");
-// }
-// }
-
-const BMIcalc = (weight, height) => {
-  let BMI = weight / (height * height);
+function BMIcalc(weight, height) {
+  let BMI = weight / sqr(height);
   console.log(BMI);
   if (BMI < 14) {
     console.log("consult doc");
@@ -38,7 +27,6 @@ const BMIcalc = (weight, height) => {
   } else {
     console.log("Just Correct");
   }
-};
+}
 
-BMIcalc(68, 1.8);
-BMIcalc(70, 1.2);
+BMIcalc(68,1.8)
