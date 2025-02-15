03446f42adfec569adde098e72bb25373c915bd8
LivinkumarS <001liv100@ggmail.com>
2025-02-15 12:08:27 +0530

    Loops-While and For loops

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-let person = {
-  name: "Pratap Poojari",
-  Role: "Developer",
-  Company: "TCS",
-  Qualification: "BE",
-  age: 28,
-  isMarried: false,
+// For Loop
 
-  printFullname: function () {
-    console.log(`His name is ${this.name} and his age is ${this.age}`);
-  },
-  printRole: () => {
-    console.log("Developer in TCS");
-  },
+// for (let i = 100; i > 0; i--) {
+//   console.log("hello",i);
+// }
 
-  object: {
-    favMovie: "KGF",
-    favFood: "Biriyani",
-    innerObject: {
-      favCartoon: "Mr Been",
-    },
-  },
-};
+// console.log("Loop ended!");
 
-delete person.object.favFood;
+// loop1: i=0
+// loop2: i=1
 
-person.printFullname();
+// .
+// .
+// .
+// loop100: i=99
+
+// 1oop101: i=100
+
+//100 time
+
+// While loop
+
+let number = 5;
+let guess; //undefined
+
+while (guess != number) {
+  guess = Number(prompt("Put a number here:"));
+
+  if (guess === number) {
+    console.log("you are correct");
+  } else {
+    console.log("try again");
+  }
+}
+
+
+//number of iteratoion is not known here.
+
