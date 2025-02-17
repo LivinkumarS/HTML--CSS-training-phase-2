37229e1ca9f97817f5d108c8338b8c211a0b3467
LivinkumarS <001liv100@ggmail.com>
2025-02-17 11:33:51 +0530

    Arrays and Array methods

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
-if(9-2+7-14){
-  console.log("Hello World");
-}
+// Array
+
+let arr2 = [5, 6, 7, 8];
+
+// console.log(arr1[12]["role"]);
+// arr1[0]=10
+// console.log(arr1);
+
+// arr1[14]()
+
+// Array Methods
+
+// Push
+
+// arr1.push(4);
+// arr2.push(10);
+
+// arr1.push(55);
+
+// arr1.pop();
+// arr2.pop();
+
+// arr1.unshift(10)
+// arr2.unshift("string")
+
+// arr1.shift();
+// arr2.shift();
+
+// let str1="surya"
+// let str2="kumar"
+
+// let newArr = arr1.concat(arr2);
+
+// console.log(arr1);
+// console.log(arr2);
+
+// console.log(newArr);
+
+// console.log(arr1.join(","));
+
+// console.log(arr1.slice(4,8));
+
+// arr1.splice(3,2,"Added elements")
+
+// arr2.splice(1,3,"added elements")
+
+// arr1.splice(4,1,100)
+
+// console.log(arr1);
+// console.log(arr2);
+
+let arr1 = [1, 2, 3, 32, "add", 4, 32, 34, 43, 35432, 32];
+
+arr1.splice(4, 1, "add");
+
+console.log(arr1.indexOf(100));  //-1
+
+
