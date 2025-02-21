b09075fa603a0e04c31f3383c12ec2c819383fea
LivinkumarS <001liv100@ggmail.com>
2025-02-21 11:34:34 +0530

    Sample Assignment

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const h2 = document.querySelector("h2");
+const height = Number(prompt("Enter Your Height"));
 
-const num = Number(prompt("Type a Number:"));
+const h1=document.querySelector("h1")
 
-if (num > 10) {
-  h2.style.backgroundColor = "red";
-  h2.style.fontSize = "larger";
-  h2.style.color = "blue";
+if(height>180){
+    h1.textContent="Over Height"
+    h1.style.backgroundColor="red"
+}else if(height>160){
+    h1.textContent="Correct Heght"
+    h1.style.backgroundColor="green"
+}else{
+    h1.textContent="Under height"
+    h1.style.backgroundColor="red"
 }
+
+
+//create BMI Calculator.
