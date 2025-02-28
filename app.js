248a3535439d6dc22b708f484534a66680340785
LivinkumarS <001liv100@ggmail.com>
2025-02-28 12:04:07 +0530

    DIce Updated with setTimeOut

--- app.js
+++ app.js
@@ -16,26 +16,28 @@ btn2.addEventListener("click", () => {
   dice2.classList.add("spinning-dice");
   random2 = Math.floor(Math.random() * 6) + 1;
 
-  if (random1 && random2) {
-    dice1.textContent = random1;
-    dice2.textContent = random2;
-
-    if (random1 > random2) {
-      h1.textContent = "Player 1 is the winner!";
-    } else if (random1 === random2) {
-      h1.textContent = "Its a Tie!";
+  setTimeout(() => {
+    if (random1 && random2) {
+      dice1.textContent = random1;
+      dice2.textContent = random2;
+  
+      if (random1 > random2) {
+        h1.textContent = "Player 1 is the winner!";
+      } else if (random1 === random2) {
+        h1.textContent = "Its a Tie!";
+      } else {
+        h1.textContent = "Player 2 is the winner!";
+      }
     } else {
-      h1.textContent = "Player 2 is the winner!";
+      h1.textContent = "Both Should Play!";
     }
-  } else {
-    h1.textContent = "Both Should Play!";
-  }
-
-  dice1.classList.remove("spinning-dice");
-  dice2.classList.remove("spinning-dice");
-
-  random1 = 0;
-  random2 = 0;
+  
+    dice1.classList.remove("spinning-dice");
+    dice2.classList.remove("spinning-dice");
+  
+    random1 = 0;
+    random2 = 0;
+  }, 3000);
 });
 
 
