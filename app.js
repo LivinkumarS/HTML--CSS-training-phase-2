304bec0e6eeeaaf437133612da328a2a7469028b
LivinkumarS <001liv100@ggmail.com>
2025-02-28 12:00:19 +0530

    Dice mini task

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-const btn = document.querySelector("button");
-const inp = document.querySelector("#year");
-const h2=document.querySelector("h2")
+const btn1 = document.querySelector(".button1");
+const btn2 = document.querySelector(".button2");
+const dice1 = document.querySelector(".dice1");
+const dice2 = document.querySelector(".dice2");
+const h1 = document.querySelector("h1");
 
-btn.addEventListener("click", () => {
-  if (inp.value % 100 == 0) {
-    if (inp.value % 400 == 0) {
-      h2.textContent="Leap Year"
+let random1 = 0; //1-6
+let random2 = 0;
+
+btn1.addEventListener("click", () => {
+  dice1.classList.add("spinning-dice");
+  random1 = Math.floor(Math.random() * 6) + 1;
+});
+
+btn2.addEventListener("click", () => {
+  dice2.classList.add("spinning-dice");
+  random2 = Math.floor(Math.random() * 6) + 1;
+
+  if (random1 && random2) {
+    dice1.textContent = random1;
+    dice2.textContent = random2;
+
+    if (random1 > random2) {
+      h1.textContent = "Player 1 is the winner!";
+    } else if (random1 === random2) {
+      h1.textContent = "Its a Tie!";
     } else {
-      h2.textContent="Non Leap Year"
+      h1.textContent = "Player 2 is the winner!";
     }
   } else {
-    if (inp.value % 4 === 0) {
-      h2.textContent="Leap Year"
-    } else {
-      h2.textContent="Non Leap Year"
-    }
+    h1.textContent = "Both Should Play!";
   }
+
+  dice1.classList.remove("spinning-dice");
+  dice2.classList.remove("spinning-dice");
+
+  random1 = 0;
+  random2 = 0;
 });
+
+
+
